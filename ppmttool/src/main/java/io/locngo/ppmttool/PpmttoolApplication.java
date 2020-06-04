03b286a1262cec31c1474d7ad9cef9220e8951b2
Daniel Ngo <danielngo1998@gmail.com>
2020-06-04 10:33:16 +0700

    first commit

---
+++ ppmttool/src/main/java/io/locngo/ppmttool/PpmttoolApplication.java
@@ -0,0 +1,13 @@
+package io.locngo.ppmttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmttoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmttoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/locngo/ppmttool/PpmttoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.locngo.ppmttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmttoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
