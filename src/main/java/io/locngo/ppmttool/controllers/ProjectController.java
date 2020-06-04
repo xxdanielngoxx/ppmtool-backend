bae7239d2adff40cc377742976b046d58b9cb4e9
Daniel Ngo <danielngo1998@gmail.com>
2020-06-04 14:36:01 +0700

    Project validation part 2

--- src/main/java/io/locngo/ppmttool/controllers/ProjectController.java
+++ src/main/java/io/locngo/ppmttool/controllers/ProjectController.java
@@ -5,12 +5,15 @@ import io.locngo.ppmttool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/projects")
@@ -26,7 +29,11 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
         if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid project object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error: result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
         project = this.projectService.upsertProject(project);
