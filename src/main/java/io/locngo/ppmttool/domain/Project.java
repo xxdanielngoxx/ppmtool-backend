386f83f5a7b3701c516f0903cfdaface2100dd7e
Daniel Ngo <danielngo1998@gmail.com>
2020-06-04 13:47:39 +0700

    Setup project validation

--- src/main/java/io/locngo/ppmttool/domain/Project.java
+++ src/main/java/io/locngo/ppmttool/domain/Project.java
@@ -1,6 +1,10 @@
 package io.locngo.ppmttool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -10,18 +14,27 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project name is required")
     private String projectName;
 
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 5, message = "Please use u to 5 character")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
 
+    @NotBlank(message = "Description is required")
     private String description;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     public Project() {
